723734244e859980eabef4819516e4b1f2b93d3b
ekelseya <42783392+ekelseya@users.noreply.github.com>
2020-08-26 11:17:07 -0600

    Update admin.py
    Register models to admin

--- patterns/admin.py
+++ patterns/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import PatternType, PatternCompany, Pattern, PatternView, Project
+
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+
+
+@admin.register(PatternView)
+class PatternViewAdmin(admin.ModelAdmin):
+    inlines = [ProjectInline]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'pattern', 'start_date', 'finish_date')
+    fields = ['owner', 'pattern', 'fabric', ('start_date', 'finish_date'), 'notes']
+
 
 # Register your models here.
+admin.site.register(PatternType)
+admin.site.register(PatternCompany)
+admin.site.register(Pattern)
